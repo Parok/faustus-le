8d4daf2e0a8101199ee29748b083bbed184d86ee
Parok <prkaestner@googlemail.com>
2024-11-07 23:41:52 +0100

    added dark style

--- theme/header.php
+++ theme/header.php
@@ -6,12 +6,19 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <header class="site-header">
-        <div class="container">
-            <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-            <p><?php bloginfo('description'); ?></p>
-            <nav>
-                <?php wp_nav_menu(array('theme_location' => 'main-menu')); ?>
-            </nav>
-        </div>
-    </header>
+<header class="site-header">
+    <div class="container">
+        <h1 class="logo">
+            <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+        </h1>
+        <nav class="main-navigation">
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'main-menu',
+                'container' => false,
+                'menu_class' => 'nav-menu'
+            ));
+            ?>
+        </nav>
+    </div>
+</header>
