6806f17caa4032f4dbd4a6ec8dca89ede6a6acd7
Parok <prkaestner@googlemail.com>
2024-11-13 00:25:20 +0100

    another approach, grid with rotating opacities

--- theme/header.php
+++ theme/header.php
@@ -8,20 +8,20 @@
 <body <?php body_class(); ?>>
 <header class="site-header header-background">
     <div class="container">
-    <div class="logo-container">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/faustus-logo.jpeg" alt="Faustus Logo" class="logo">
-        <div class="fog-overlay"></div>
-    </div>
-    
-    <nav class="main-navigation">
-        <?php
-        wp_nav_menu(array(
-            'theme_location' => 'main-menu',
-            'container' => false,
-            'menu_class' => 'nav-menu'
-        ));
-        ?>
-    </nav>
+        <div class="logo-container">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/faustus-logo.jpeg" alt="Faustus Logo" class="logo">
+            <div class="fog-grid"></div>
+        </div>
+
+        <nav class="main-navigation">
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'main-menu',
+                'container' => false,
+                'menu_class' => 'nav-menu'
+            ));
+            ?>
+        </nav>
     </div>
 </header>
 </body>
