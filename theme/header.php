353f6a59e1dd8d456b073c8c182b2a73fd39b60d
Parok <prkaestner@googlemail.com>
2024-11-13 01:51:14 +0100

    changed lib src

--- theme/header.php
+++ theme/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://cdn.jsdelivr.net/npm/simplex-noise"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simplex-noise@2.4.0/simplex-noise.min.js"></script>
 
     <?php wp_head(); ?>
 </head>
