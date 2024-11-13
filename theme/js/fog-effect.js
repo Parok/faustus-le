756f8c1d29d6f15dae9ff961bf3a20e8f3a71eba
Parok <prkaestner@googlemail.com>
2024-11-13 03:18:25 +0100

    adjusting cell size

--- theme/js/fog-effect.js
+++ theme/js/fog-effect.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const simplex = new SimplexNoise();
     console.log("SimplexNoise object:", simplex);
-    const cellSize = 20;
+    const cellSize = 30;
 
     function createFogCells() {
         fogGrid.innerHTML = '';
